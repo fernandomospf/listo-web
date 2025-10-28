37b1a35d70db970013ad972af034b236e3e2bb1e
Fernando Mos <fernandomosp@gmail.com>
2025-10-27 22:27:20 -0300

    [UPDATE] style

--- app/components/Login/Login.style.ts
+++ app/components/Login/Login.style.ts
@@ -8,33 +8,43 @@ const InputLogin = styled(TextField) <TextFieldProps>`
     
     & .MuiOutlinedInput-notchedOutline {
       border: 1px solid #1F2937;
+      background-color: transparent;
     }
     
     &:hover .MuiOutlinedInput-notchedOutline {
       border-color: #1F2937;
+      background-color: transparent;
     }
     
     &.Mui-focused .MuiOutlinedInput-notchedOutline {
       border-color: #1F2937; 
       border-width: 2px;
+      background-color: transparent;
     }
     
     & .MuiInputBase-input {
       color: #1F2937;
       background-color: transparent;
+      
+      &::placeholder {
+        color: #6B7280;
+      }
     }
   }
   
   & .MuiInputLabel-root {
     color: #1F2937;
+    background-color: transparent;
     
     &.Mui-focused {
       color: #1F2937;
+      background-color: transparent;
     }
   }
   
   & .MuiFormHelperText-root {
     color: #1F2937;
+    background-color: transparent;
   }
 `;
 const LoginButton = styled(Button) <ButtonProps>`
